2637066330165d4867f81fb988de7a154c6433cc
Patricio <sessaregopatricio@gmail.com>
2024-08-23 20:29:01 -0300

    V2

--- TP2_GRUPO_8/TP2_GRUPO_8/Ejercicio1.aspx.designer.cs
+++ TP2_GRUPO_8/TP2_GRUPO_8/Ejercicio1.aspx.designer.cs
@@ -24,21 +24,39 @@ namespace TP2_GRUPO_8
         protected global::System.Web.UI.HtmlControls.HtmlForm form1;
 
         /// <summary>
-        /// Control lblNombreProducto.
+        /// Control lblNombreProductoUno.
         /// </summary>
         /// <remarks>
         /// Campo generado automáticamente.
         /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
         /// </remarks>
-        protected global::System.Web.UI.WebControls.Label lblNombreProducto;
+        protected global::System.Web.UI.WebControls.Label lblNombreProductoUno;
 
         /// <summary>
-        /// Control txtNombreProducto.
+        /// Control txtNombreProductoUno.
         /// </summary>
         /// <remarks>
         /// Campo generado automáticamente.
         /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
         /// </remarks>
-        protected global::System.Web.UI.WebControls.TextBox txtNombreProducto;
+        protected global::System.Web.UI.WebControls.TextBox txtNombreProductoUno;
+
+        /// <summary>
+        /// Control lblNombreProductoDos.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.Label lblNombreProductoDos;
+
+        /// <summary>
+        /// Control txtNombreProductoDos.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.TextBox txtNombreProductoDos;
     }
 }
