fe7d76d517ff57c7fc5f1adca5b13c202a1a8e3f
KaliWick <168383538+KaliWick@users.noreply.github.com>
2024-08-23 08:59:57 -0300

    V1
    Formulariso vacios y ejercicios 1 y 2

---
+++ TP2_GRUPO_8/TP2_GRUPO_8/Ejercicio1.aspx
@@ -0,0 +1,20 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="Ejercicio1.aspx.cs" Inherits="TP2_GRUPO_8.Ejercicio1" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8"/>
+    <title></title>
+</head>
+<body>
+    <form id="form1" runat="server">
+        <div>
+        </div>
+        <asp:Label ID="lblNombreProducto" runat="server" Text="Ingrese nombre del producto: "></asp:Label>
+        <asp:TextBox ID="txtNombreProducto" runat="server" Height="16px" Width="144px"></asp:TextBox>
+        <p>
+            &nbsp;</p>
+    </form>
+</body>
+</html>
---
+++ TP2_GRUPO_8/TP2_GRUPO_8/Ejercicio2A.aspx.designer.cs
@@ -0,0 +1,44 @@
+﻿//------------------------------------------------------------------------------
+// <generado automáticamente>
+//     Este código fue generado por una herramienta.
+//
+//     Los cambios en este archivo podrían causar un comportamiento incorrecto y se perderán si
+//     se vuelve a generar el código. 
+// </generado automáticamente>
+//------------------------------------------------------------------------------
+
+namespace TP2_GRUPO_8
+{
+
+
+    public partial class Ejercicio2A
+    {
+
+        /// <summary>
+        /// Control form2A.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::System.Web.UI.HtmlControls.HtmlForm form2A;
+
+        /// <summary>
+        /// Control lblNombre.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.Label lblNombre;
+
+        /// <summary>
+        /// Control txtNombre.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.TextBox txtNombre;
+    }
+}
