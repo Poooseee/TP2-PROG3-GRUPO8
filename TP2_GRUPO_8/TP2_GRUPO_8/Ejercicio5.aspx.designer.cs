35d4b45cf1a7754b35d15513c2db97d8c88ecb1f
IvanMoreiraUTN <ivan.moreira2@alumnos.frgp.utn.edu.ar>
2024-08-26 20:12:55 -0300

    Primer Aporte ej 5
    Agregué el lbl de accesorios y el checkboxlist con sus respectivos items

--- TP2_GRUPO_8/TP2_GRUPO_8/Ejercicio5.aspx.designer.cs
+++ TP2_GRUPO_8/TP2_GRUPO_8/Ejercicio5.aspx.designer.cs
@@ -49,5 +49,23 @@ namespace TP2_GRUPO_8
         /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
         /// </remarks>
         protected global::System.Web.UI.WebControls.DropDownList ddlMemoria;
+
+        /// <summary>
+        /// Control lblAccesorios.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.Label lblAccesorios;
+
+        /// <summary>
+        /// Control CBLAccesorios.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.CheckBoxList CBLAccesorios;
     }
 }
